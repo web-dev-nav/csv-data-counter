faac11e5508f48a9bda06faaff64a6e7623a07fe
Codelone <110724391+web-dev-nav@users.noreply.github.com>
2023-10-31 22:18:37 -0400

    Update script.js
    added comments

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
+// Wait for the DOM to be fully loaded before running the script
 document.addEventListener("DOMContentLoaded", function () {
+    // Get references to the HTML elements you'll be working with
     const countButton = document.getElementById("countButton");
     const rowCount = document.getElementById("rowCount");
     const countValue = document.getElementById("countValue");
 
+    // Add a click event listener to the "countButton" element
     countButton.addEventListener("click", function () {
-        // Replace 'your-csv-file.csv' with the path to your CSV file on the server.
+        // Replace 'open_signuplist.csv' with the path to your CSV file on the server.
         const csvFile = 'open_signuplist.csv';
 
+        // Fetch the CSV file
         fetch(csvFile)
-            .then(response => response.text())
+            .then(response => response.text()) // Read the response as text
             .then(csvData => {
-                const rows = csvData.split("\n"); // Split the CSV into rows
-                const rowCount = rows.length - 1; // Subtract 1 to exclude header row
+                // Split the CSV data into rows using line breaks
+                const rows = csvData.split("\n");
 
+                // Calculate the row count by subtracting 1 (for the header row)
+                const rowCount = rows.length - 1;
+
+                // Update the text content of the "countValue" element with the row count
                 countValue.textContent = rowCount;
+
+                // Make the "rowCount" element visible (assuming it was previously hidden)
                 rowCount.style.display = "block";
             })
             .catch(error => {
+                // Handle errors, such as the file not being found or network issues
                 console.error("Error reading CSV file:", error);
             });
     });
